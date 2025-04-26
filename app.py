d73c108f4c38d928d4c8ff2915fb353a4390ff4d
LeoM030 <leomuliarchuk51@gmail.com>
2025-04-26 21:47:03 +0300

    create next step

--- app.py
+++ app.py
@@ -4,11 +4,15 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher, types
-
+from aiogram.filters import CommandStart
 
 bot = Bot(token="")
-
 dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer("Hello world")
+
 
 async def main():
     await dp.start_polling(bot)
