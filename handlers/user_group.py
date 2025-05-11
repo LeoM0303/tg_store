90c98944477364bf80d2715fc86501b83e0980d4
LeoM030 <leomuliarchuk51@gmail.com>
2025-05-11 12:59:17 +0300

    add new file, with filter for group

---
+++ handlers/user_group.py
@@ -0,0 +1,15 @@
+from string import punctuation
+
+from aiogram import F, types, Router
+
+user_group_router = Router()
+
+restricted_words = {'fuck', 'shit', 'your mom'}
+
+def cleaner_text(text: str):
+    return text.translate(str.maketrans('', '', punctuation))
+
+@user_group_router.message()
+async def start_cmd(message: types.Message):
+    if restricted_words.intersection(message.text.lower().split()):
+        await message.delete()
